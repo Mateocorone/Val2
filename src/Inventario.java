9952a0cbf58b7ae5775b3e37bc9718a6b95e6a26
Mateocorone <105876437+Mateocorone@users.noreply.github.com>
2023-06-09 05:53:48 -0500

    Update Inventario.java

--- src/Inventario.java
+++ src/Inventario.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Inventario {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Inventario {
+    private Map<String, Integer> medicinas;
+
+    public Inventario() {
+        medicinas = new HashMap<String, Integer>();
+    }
+
+    public void agregarMedicina(String nombre, int cantidad) {
+        if (medicinas.containsKey(nombre)) {
+            System.out.println("Error: la medicina ya existe en el inventario.");
+        } else {
+            medicinas.put(nombre, cantidad);
+        }
+    }
+
 }
